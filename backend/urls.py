bd77386c3a0de513565de2b36a41b2276ac736bc
Joel Garcia <157417367+JoelLGarcia0@users.noreply.github.com>
2025-07-24 14:52:52 -0400

    adding static urls

--- backend/urls.py
+++ backend/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("store/", include("store.urls")),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
